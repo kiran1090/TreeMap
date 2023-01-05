ef010c85e5624cc3a8fb0a6bb32b657f6a58ff74
kiran_a <iamkiranashok@gmail.com>
2023-01-05 12:46:01 +0530

    First commit

--- src/HelloToMaps.java
+++ src/HelloToMaps.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class HelloToMaps {
+import java.util.TreeMap;
+public class HelloToMaps {
+
+
+    public static void main(String[] args) {
+    TreeMap<Integer,String>mymap= new TreeMap<Integer,String>();
+    mymap.put(100,"kiran");
+    mymap.put(99,"mee");
+    mymap.put(999,"nathan");
+    System.out.println(mymap);
+    }
 }
